4c58b4b2f1339f1cf9ae6ff81a63157a338abfbb
rahulk64 <emailrahulk64@gmail.com>
2018-10-20 14:49:42 -0500

    Add Random Quotes

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
-var app = angular.module('spaceApp', []);
+let app = angular.module('spaceApp', []);
 
-var randomDay = Math.floor((Math.random() * 30) + 1);
-var randomMonth = Math.floor((Math.random() * 9) + 1);
+let randomDay = Math.floor((Math.random() * 30) + 1);
+let randomMonth = Math.floor((Math.random() * 9) + 1);
 
 app.controller('spaceController', function($scope, $http, $window) {
   $http.get("https://api.nasa.gov/planetary/apod?api_key=K5L9TQ8xYzSvXS05LOu6H2GqzZ4BaDniA1amyYUP&date=2018-0" + randomMonth + "-" + randomDay)
@@ -17,3 +17,20 @@ app.controller('spaceController', function($scope, $http, $window) {
     console.log($scope.date);
   });
 });
+
+//quotes
+let quotes =
+[
+  {
+  "body": "\"Man must rise above the Earth - to the top of the atmosphere and beyond - for only thus will he fully understand the world in which he lives.\"",
+  "author": "~ Socrates, Philosopher"
+  },
+  {
+  "body": "\"Astronomy compels the soul to look upward, and leads us from this world to another.\"",
+  "author": "~ Plato, Philosopher"
+  }
+]
+
+let randomIndex = Math.floor(Math.random() * quotes.length);
+document.getElementById("quote-text").innerHTML = quotes[randomIndex].body;
+document.getElementById("author").innerHTML = quotes[randomIndex].author;
