28747f7474e880a03cfb1e62f8fd55be99997a90
rahulk64 <emailrahulk64@gmail.com>
2018-10-20 14:13:49 -0500

    Add Time Functionality

---
+++ scripts/time.js
@@ -0,0 +1,32 @@
+var name = "Timothy"
+setInterval(function() {
+   var time = new Date();
+   var hour = time.getHours();
+   var minute = time.getMinutes();
+
+   if(hour >= 18) {
+     greeting = "Good Evening";
+   } else if (hour >= 12) {
+     greeting = "Good Afternoon";
+   } else if(hour > 0) {
+     greeting = "Good Morning";
+   } else {
+     greeting = "Welcome";
+   }
+   $("#welcome-text").text(greeting + ", " + name + "!");
+
+   if(hour > 12) {
+     hour -= 12;
+   }
+
+   if(hour >= 10 && minute >= 10) {
+     $("#time-text").text(hour.toString() + ":" + minute.toString());
+   } else if (hour >= 10) {
+     $("#time-text").text(hour.toString() + ":0" + minute.toString());
+   } else if (minute >= 10) {
+     $("#time-text").text('0'+ hour.toString() + ":" + minute.toString());
+   } else {
+     $("#time-text").text('0'+ hour.toString() + ":0" + minute.toString());
+   }
+   setTimeout(test, 5000);
+}, 50);
